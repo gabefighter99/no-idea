e48d9b505e59278800465b0764662e1897256412
gabe.petrov <gabe.petrov@uber.com>
2024-05-19 16:03:38 +0300

    fix

--- backend/hello-world.go
+++ backend/hello-world.go
@@ -9,6 +9,7 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		fmt.Fprintf(w, "Hello World")
+		fmt.Fprintf(w, "Alvin is a bitch")
 	default:
 		http.Error(w, "Invalid request method.", 405)
 	}
