ebf46698560e3c74924c1a5d9ed4172d95147458
gabe.petrov <gabe.petrov@uber.com>
2024-08-20 20:26:45 +0300

    Reorganize

--- frontend/hello-world/src/components/Stage.tsx
+++ frontend/hello-world/src/components/Stage.tsx
@@ -1,20 +1,13 @@
 import { Stage, Layer, Rect, Circle, Transformer, Line } from "react-konva";
 import { useEffect, useRef, useState } from "react";
 import Konva from "konva";
-import {
-  TOOLS,
-  RectType,
-  CircleType,
-  LineType,
-  TextType,
-  ACTION,
-  COLORS,
-} from "./constants";
+import { TextType, ACTION, COLORS, TOOLS } from "./constants";
 import Toolbar from "./Toolbar";
 import EditableText from "./editable-text/EditableText";
 import { Html } from "react-konva-utils";
 import LineArrow from "./shapes/LineArrow";
 import { handleMouseOut, handleMouseOver } from "./eventHandlers";
+import useDrawingTool from "../hooks/useDrawingTool";
 
 export default function StageComponent() {
   const stageRef = useRef<Konva.Stage>(null);
@@ -25,124 +18,24 @@ export default function StageComponent() {
   const [color, setColor] = useState("#000000");
   const [selected, setSelected] = useState<Konva.Node | null>(null);
 
-  useEffect(() => {
-    trRef.current?.nodes([]);
-    if (!selected) {
-      document.body.style.cursor = "default";
-    } else if (selected.name() !== "LineArrow") {
-      trRef.current?.nodes([selected]);
-    }
-  }, [selected]);
-
   const action = useRef(ACTION.NONE);
   const isDraggable = tool === TOOLS.HAND;
 
-  const [rects, setRects] = useState<RectType[]>([]);
-  const [circles, setCircles] = useState<CircleType[]>([]);
-  const [lines, setLines] = useState<LineType[]>([]);
-  const [scribbles, setScribbles] = useState<LineType[]>([]);
   const [texts, setTexts] = useState<TextType[]>([]);
-
-  const handlePtrDown = () => {
-    if (tool === TOOLS.HAND || tool === TOOLS.TEXT) return;
-    if (!stageRef.current) return;
-
-    action.current = ACTION.DRAWING;
-
-    const stage = stageRef.current;
-    const pos = stage.getPointerPosition();
-
-    if (!pos) return;
-
-    const id = Date.now().toString();
-
-    switch (tool) {
-      case TOOLS.RECT:
-        setRects([
-          ...rects,
-          { id, x: pos.x, y: pos.y, height: 0, width: 0, rotate: 0, color },
-        ]);
-        break;
-      case TOOLS.DIAMOND:
-        setRects([
-          ...rects,
-          { id, x: pos.x, y: pos.y, height: 0, width: 0, rotate: 45, color },
-        ]);
-        break;
-      case TOOLS.CIRCLE:
-        setCircles([...circles, { id, x: pos.x, y: pos.y, radius: 0, color }]);
-        break;
-      case TOOLS.LINE:
-      case TOOLS.ARROW:
-        setLines([
-          ...lines,
-          { id, isArrow: tool === TOOLS.ARROW, points: [pos.x, pos.y], color },
-        ]);
-        break;
-      case TOOLS.SCRIBBLE:
-        setScribbles([
-          ...scribbles,
-          { id, isArrow: false, points: [pos.x, pos.y], color },
-        ]);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handlePtrUp = () => {
-    if (tool === TOOLS.HAND || action.current !== ACTION.DRAWING) return;
-    action.current = ACTION.NONE;
-    setTool(TOOLS.HAND);
-  };
-
-  const handlePtrMove = () => {
-    if (tool === TOOLS.HAND || action.current !== ACTION.DRAWING) return;
-    if (!stageRef.current) return;
-
-    const stage = stageRef.current;
-    const pos = stage.getPointerPosition();
-
-    if (!pos) return;
-
-    switch (tool) {
-      case TOOLS.RECT:
-      case TOOLS.DIAMOND:
-        let rect = rects[rects.length - 1];
-        rect.height = pos.y - rect.y;
-        rect.width = tool === TOOLS.RECT ? pos.x - rect.x : rect.height;
-
-        rects.splice(rects.length - 1, 1, rect);
-        setRects(rects.concat());
-        break;
-      case TOOLS.CIRCLE:
-        let circle = circles[circles.length - 1];
-        circle.radius = Math.sqrt(
-          (pos.x - circle.x) ** 2 + (pos.y - circle.y) ** 2,
-        );
-
-        circles.splice(circles.length - 1, 1, circle);
-        setCircles(circles.concat());
-        break;
-      case TOOLS.SCRIBBLE:
-        let scribble = scribbles[scribbles.length - 1];
-        scribble.points = scribble.points.concat([pos.x, pos.y]);
-
-        scribbles.splice(scribbles.length - 1, 1, scribble);
-        setScribbles(scribbles.concat());
-        break;
-      case TOOLS.LINE:
-      case TOOLS.ARROW:
-        let line = lines[lines.length - 1];
-        line.points = [line.points[0], line.points[1], pos.x, pos.y];
-
-        lines.splice(lines.length - 1, 1, line);
-        setLines(lines.concat());
-        break;
-      default:
-        break;
-    }
-  };
+  const [clickCooldown, setClickCooldown] = useState(false);
+  const {
+    rects,
+    circles,
+    lines,
+    scribbles,
+    handlePtrDown,
+    handlePtrMove,
+    handlePtrUp,
+    setRects,
+    setCircles,
+    setLines,
+    setScribbles,
+  } = useDrawingTool(stageRef, action, color, tool, setTool, setClickCooldown);
 
   const handleSelect = (e: Konva.KonvaEventObject<MouseEvent>) => {
     if (tool !== TOOLS.HAND) return;
@@ -150,6 +43,7 @@ export default function StageComponent() {
   };
 
   const handleDblTapClick = () => {
+    if (clickCooldown) return;
     if (!stageRef.current) return;
 
     setSelected(null);
@@ -191,6 +85,15 @@ export default function StageComponent() {
   const handleSave = () => {
     const uri = stageRef?.current?.toBlob;
   };
+
+  useEffect(() => {
+    trRef.current?.nodes([]);
+    if (!selected) {
+      document.body.style.cursor = "default";
+    } else if (selected.name() !== "LineArrow") {
+      trRef.current?.nodes([selected]);
+    }
+  }, [selected]);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
